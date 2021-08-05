5af4cc22b55fd6fb942d53ceed005899deeddbc3
unknown <jobs.pradeep@hotmail.com>
2021-08-05 11:45:09 -0400

    added azure resource group

---
+++ output.tf
@@ -0,0 +1,8 @@
+output "ResourceGroupId" {  
+    description = "resource group id"  
+    value = azurerm_resource_group.rg.id  
+}  
+output "ResourceGroupName" {  
+    description = "resource group name"  
+    value = azurerm_resource_group.rg.name 
+}  
